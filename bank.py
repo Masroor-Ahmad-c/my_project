d1bc7409eb03bea50978d3645194c255405d6dc8
amna <mass@gmail.com>
2025-03-04 13:48:50 +0000

    bank.py,fixed space error using .strip()...and provided case _

--- bank.py
+++ bank.py
@@ -1,17 +1,19 @@
 
 def bank(action,tot_bal):
-    match action:
+    match action.strip(): #  it will strip space value in both end
         case "deposit":
             amount=int(input("enter amount:"))
-            print(tot_bal + amount)
+            print(f"total balance is {tot_bal + amount}")
         case "withdraw":
             amount=int(input("enter amount:"))
             if tot_bal<amount:
                 print("insufficient bank balance")
             else:
-                print(tot_bal - amount)
+                print(f"total balance is {tot_bal - amount}")
         case "balance":
             print(tot_bal)
+        case _:
+            print("enter valid action :(")
 
 def main():
     bal=int(input("tot bal:"))
